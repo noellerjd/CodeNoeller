12e1284e3c6e48c87918ddef1d80059c0433b5ce
noellerjd <noellerninja@gmail.com>
2023-02-14 21:21:45 -0700

    Added blog page and did styling for the posts

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,43 @@
 import React from "react";
+import "../assets/about.css";
 
 function About() {
-  return <h2>About</h2>;
+  return (
+    <div className="about-container">
+      <h1 className="about-heading">
+        Justin<span> Noeller</span>
+      </h1>
+      <p className="about-description">
+        Hey there! I'm Justin Noeller, a full-stack developer based in Colorado
+        and I'm excited to welcome you to my React Coding Blog/Portfolio.
+      </p>
+      <p className="about-description">
+        I recently graduated from Denver University's Full-Stack Development
+        Boot Camp, and I've been passionate about programming since I was a
+        child. I grew up in Colorado Springs and have been living in this
+        beautiful state all my life.
+      </p>
+      <p className="about-description">
+        In my free time, you can catch me coding with my friends, playing video
+        games, rock climbing, and hanging out with my fiance Bri and my corgi
+        Ein.
+      </p>
+      <p className="about-description">
+        When it comes to coding, I'm dedicated to creating innovative,
+        user-friendly web applications that bring real value to users.
+      </p>
+      <p className="about-description">
+        My primary focus is to foster collaborative environments and cultivate
+        continuous learning. I believe that working together and staying curious
+        are the key ingredients to success in the ever-evolving world of
+        technology.
+      </p>
+      <p className="about-description">
+        Thank you for stopping by, and I hope you enjoy exploring my blog and
+        portfolio!
+      </p>
+    </div>
+  );
 }
 
 export default About;
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,41 @@
 import React from "react";
+import "../assets/blog.css";
 
-function Blog() {
-  return <h2>Blog</h2>;
-}
+const Blog = () => {
+  const posts = [
+    {
+      author: "Justin Noeller",
+      created: "2023-02-13",
+      title: "Hello World!",
+      content:
+        "Managed to get the posts to display how i'd like them, will have to see as time goes on... may need a sorting feature in the future.",
+    },
+    {
+      author: "Justin Noeller",
+      created: "2023-02-14",
+      title: "Backend Success!",
+      content: `While I may be a full-stack developer. I do sometimes feel a little to biased towards front-end work. That being said when my friend, Dylan, asked if I could help with some backend work, I jumped at the chance to refine my skills! The code was only to fetch data from his SQL database and track player data for a minecraft server plugin he's working on, but I managed to make it work and now I look forward to utilizing this through a website I'll be building for players to track different stats!`,
+    },
+  ];
+
+  return (
+    <div className="blog-container">
+      <div className="blog-posts-container">
+        {posts.map((post, index) => (
+          <div className="blog-post" key={index}>
+            <h2 className="blog-title">{post.title}</h2>
+            <div className="blog-author">
+              By <span>{post.author}</span>
+            </div>
+            <div className="blog-created">
+              Created on <span>{post.created}</span>
+            </div>
+            <div className="blog-content">{post.content}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
